759fa46988be56d5bba22bdb1cc96ff5ce772a06
Wesley Scholl <clients@konjoweb.com>
2023-02-12 14:16:44 -0500

    tested, text now passes to twitter post

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import quoteArr from './quotes.js'
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [quotes, setQuotes] = useState(quoteArr)
+  const [quotes] = useState(quoteArr)
   const [quote, setQuote] = useState('')
 
   useEffect(() => {
-    // setQuotes(quoteArr)
     getNewQuote()
+    // eslint-disable-next-line
   }, []);
 
   const getNewQuote = () => {
@@ -31,7 +31,7 @@ function App() {
           <p id="author" style={{textAlign: 'right', color: '#777', paddingRight: 25, paddingBottom: 15}}>- {quote && quote.author}</p>
           <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', paddingRight: 20, paddingLeft: 20}}>
           <div style={{display: 'flex',justifyContent: 'flex-start'}}>
-          <a id="tweet-quote" target="_blank" rel="noreferrer" href='https://twitter.com/intent/tweet'><TwitterIcon sx={{ fontSize: 40, color: '#000', opacity: 0.4 }}  /></a>
+          <a id="tweet-quote" target="_blank" rel="noreferrer" href={`https://twitter.com/intent/tweet?text=${quote.quote + " -" + quote.author}`}><TwitterIcon sx={{ fontSize: 40, color: '#000', opacity: 0.4 }}  /></a>
           <img src={require('./tumblr.png')} alt='tumblr' style={{height: 40, opacity: 0.4}}/>
           </div>
           <Button id="new-quote" variant="contained" size="large" style={{ marginBottom: 25}} onClick={()=> getNewQuote()}>
