f8eaec6e3f10cbe4b1a7e2abc81fec0e96bc8d09
Wesley Scholl <clients@konjoweb.com>
2023-02-12 13:36:45 -0500

    minimum ui, adding functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 
 import './App.css';
 import Button from '@mui/material/Button';
-import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div id="quote-box" style={{boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px', width: 650, borderRadius: 8, padding: 20}}>
-          <h2 style={{color: '#555', padding: 20}}>Random Quote Generator</h2>
-          <p style={{color: '#777', paddingRight: 20, paddingLeft: 20}}><FormatQuoteIcon sx={{ fontSize: 40, color: '#999' }}  />
+          <h2 style={{color: '#444', padding: 20, fontWeight: 300}}>Random Quote Generator</h2>
+          <p id="text" style={{color: '#777', paddingRight: 20, paddingLeft: 20}}><img src={require('./quotes.png')} alt='tumblr' style={{height: 35, padding: 8, opacity: 0.4}}/>
           You may be disappointed if you fail, but you are doomed if you don't try.</p>
-          <p style={{textAlign: 'right', color: '#777', paddingRight: 25}}>- Beverly Sills</p>
-          <Button variant="contained" size="large" style={{ marginBottom: 25}}>
+          <p id="author" style={{textAlign: 'right', color: '#777', paddingRight: 25, paddingBottom: 15}}>- Beverly Sills</p>
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', paddingRight: 20, paddingLeft: 20}}>
+          <div style={{display: 'flex',justifyContent: 'flex-start'}}>
+          <a id="tweet-quote" target="_blank" rel="noreferrer" href='https://twitter.com/intent/tweet'><TwitterIcon sx={{ fontSize: 40, color: '#000', opacity: 0.4 }}  /></a>
+          <img src={require('./tumblr.png')} alt='tumblr' style={{height: 40, opacity: 0.4}}/>
+          </div>
+          <Button id="new-quote" variant="contained" size="large" style={{ marginBottom: 25}}>
             New Quote
           </Button>
+          </div>
         </div>
       </header>
     </div>
